0ca4900622717484b245ec0c2646632e2ec48d11
romashka <roma.l@mail.ru>
2022-10-18 21:42:28 +0400

    first parsing done

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# main
+import requests
+from bs4 import BeautifulSoup
+
+def get_html():
+    url = "https://rp5.ru/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B5_(%D0%92%D0%94%D0%9D%D0%A5)"
+    r = requests.get(url)
+    return r
+
+
+def get_content(html):
+    soup = BeautifulSoup(html.content, 'html.parser')
+    weather = soup.find_all('meta')
+    return print(weather)
+
+
+html = get_html()
+get_content(html)
