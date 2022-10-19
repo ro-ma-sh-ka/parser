4f13b284ec110a797d167539f00c76ee1c808464
romashka <roma.l@mail.ru>
2022-10-19 20:08:01 +0400

    last changes

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# parser
+# <p align="center">Weather parser <a>1.0</a>
+
+<p align="center">Simple parser of the pr5.ru to show day weather forecast in Moscow</p>
+
+## Contents
+
+  * [Getting started](#getting-started)
+  * [Questions and suggestions](#questions-and-suggestions)
+
+## Getting started
+Download this Api and run
+```
+main.py
+```
+
+## Questions and suggestions
+Please send it to:
+* e-mail: roma.l@mail.ru
+* telegram: @Ro_ma_sh_ka
--- main.py
+++ main.py
@@ -2,20 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_html():
-    url = "https://rp5.ru/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B5_(%D0%92%D0%94%D0%9D%D0%A5)"
+def get_html(url: str) -> requests.models.Response:
+    """the function gets html to send it to get_content function"""
     r = requests.get(url)
     return r
 
 
-def get_content(html):
+def get_content(html: requests.models.Response) -> str:
+    """the function takes html from get_html function uses BeautifulSoup to get current weather forecast"""
     soup = BeautifulSoup(html.content, 'html.parser')
-
     for meta in soup.findAll('meta', {'name': 'description'}):
         weather = meta['content']
-
-    return print(weather)
-
-
-html = get_html()
-get_content(html)
+    return weather
