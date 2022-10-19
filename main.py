26ed5ba06b353fabc8548b9ddc80c1235aedfaf4
romashka <roma.l@mail.ru>
2022-10-19 11:07:07 +0400

    get the weather

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_html():
     url = "https://rp5.ru/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B5_(%D0%92%D0%94%D0%9D%D0%A5)"
     r = requests.get(url)
@@ -9,7 +10,10 @@ def get_html():
 
 def get_content(html):
     soup = BeautifulSoup(html.content, 'html.parser')
-    weather = soup.find_all('meta')
+
+    for meta in soup.findAll('meta', {'name': 'description'}):
+        weather = meta['content']
+
     return print(weather)
 
 
